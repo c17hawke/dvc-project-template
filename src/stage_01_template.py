961a18d4d77deae8bbd061b3a384539dc3572ccd
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-10-11 06:45:07 +0530

    stage template added

---
+++ src/stage_01_template.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+def main(config_path):
+    pass
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(">>>>> stage one started <<<<<")
+        get_data(config_path=parsed_args.config)
+        logging.info(">>>>> stage one completed! all the data are saved in local <<<<<n")
+    except Exception as e:
+        logging.exception(e)
+        raise e

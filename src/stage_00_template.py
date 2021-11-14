5e14e6b4f516d608fec61b453e27b7acec8a671a
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-11-14 23:41:47 +0530

    basic template update

--- src/stage_00_template.py
+++ src/stage_00_template.py
@@ -3,7 +3,11 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
+from src.utils.common import read_yaml, create_directories
+import random
 
+
+STAGE = "STAGE_NAME" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -12,19 +16,25 @@ logging.basicConfig(
     filemode="a"
     )
 
-def main(config_path):
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
     pass
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
-        logging.info(">>>>> stage one started <<<<<")
-        main(config_path=parsed_args.config)
-        logging.info(">>>>> stage one completed! all the data are saved in local <<<<<n")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
         raise e
---
+++ src/utils/common.py
@@ -0,0 +1,24 @@
+import os
+import yaml
+import logging
+import time
+import pandas as pd
+import json
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+    return content
+
+def create_directories(path_to_directories: list) -> None:
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        logging.info(f"created directory at: {path}")
+
+
+def save_json(path: str, data: dict) -> None:
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"json file saved at: {path}")
